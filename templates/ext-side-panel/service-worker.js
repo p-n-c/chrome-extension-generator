53be33d2baafd6790d8f288ae7aa34d65ca8bdf5
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2025-02-19 19:49:26 +0000

    fix: Use chrome.storage API for the service worker

--- templates/ext-side-panel/service-worker.js
+++ templates/ext-side-panel/service-worker.js
@@ -1,5 +1,13 @@
-let isSidePanelOpen = false
-let currentTab = undefined
+// Service workers are ephemeral, use the chrome.storage API
+// Not global variables as the source of truth in your event listeners
+const storageCache = {
+  isSidePanelOpen: false,
+  currentTab: undefined,
+}
+
+const initStorageCache = chrome.storage.local.get().then((items) => {
+  Object.assign(storageCache, items)
+})
 
 const closeSidePanel = async () => {
   chrome.runtime.sendMessage({
@@ -7,47 +15,57 @@ const closeSidePanel = async () => {
     action: 'close-side-panel',
   })
   // Let the tab content script know that the side panel is closed
-  chrome.tabs.sendMessage(currentTab.id, {
+  chrome.tabs.sendMessage(storageCache.currentTab.id, {
     from: 'service-worker',
     action: 'side-panel-close',
   })
-  currentTab = undefined
-  isSidePanelOpen = false
+  storageCache.currentTab = undefined
+  storageCache.isSidePanelOpen = false
 }
 
-chrome.action.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener(async (tab) => {
   // Visitor clicks on the extension icon
-  if (isSidePanelOpen) {
+  if (storageCache.isSidePanelOpen) {
     console.log('Closing side panel')
+    await initStorageCache
     closeSidePanel()
   } else {
-    currentTab = tab
-    console.log(`Opening side panel for tab ${currentTab.id}`)
-    chrome.sidePanel.open({ windowId: currentTab.windowId })
-    isSidePanelOpen = true
+    storageCache.currentTab = tab
+    console.log(`Opening side panel for tab ${storageCache.currentTab.id}`)
+    chrome.sidePanel.open({ windowId: storageCache.currentTab.windowId })
+    // Can't be run outside the if statement because
+    // the sidePanel.open method has to be first in the event listener
+    await initStorageCache
+    storageCache.isSidePanelOpen = true
   }
+  chrome.storage.local.set(storageCache)
 })
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  await initStorageCache
   // When the visitor updates the current tab, we keep the panel open
   if (changeInfo.status === 'complete') {
-    currentTab = tab
+    storageCache.currentTab = tab
     chrome.runtime.sendMessage({
       from: 'service-worker',
       action: 'display-message',
-      content: `Side panel for tab: ${currentTab.title}`,
+      content: `Side panel for tab: ${storageCache.currentTab.title}`,
     })
   }
+  chrome.storage.local.set(storageCache)
 })
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  await initStorageCache
   console.log(`New tab activated: ${activeInfo.tabId}`)
   // When the visitor goes to another tab, we close the panel if it's open
-  if (isSidePanelOpen) closeSidePanel()
+  if (storageCache.isSidePanelOpen) closeSidePanel()
+  chrome.storage.local.set(storageCache)
 })
 
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.from === 'side-panel')
+chrome.runtime.onMessage.addListener(async (message) => {
+  await initStorageCache
+  if (message.from === 'side-panel') {
     switch (message.action) {
       case 'loaded':
         console.log('Side panel loaded')
@@ -55,13 +73,15 @@ chrome.runtime.onMessage.addListener((message) => {
         chrome.runtime.sendMessage({
           from: 'service-worker',
           action: 'display-message',
-          content: `Side panel for tab: ${currentTab.title}`,
+          content: `Side panel for tab: ${storageCache.currentTab.title}`,
         })
         // Let the tab content script know that the user started the extension
-        chrome.tabs.sendMessage(currentTab.id, {
+        chrome.tabs.sendMessage(storageCache.currentTab.id, {
           from: 'service-worker',
           action: 'side-panel-open',
         })
         break
     }
+  }
+  chrome.storage.local.set(storageCache)
 })
